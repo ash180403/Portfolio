b70e79c85b505edc161203e348ff17fa4d3d7bce
ash180403 <ashishakteri@gmail.com>
2025-09-27 02:36:22 +0530

    new

--- api/contact.js
+++ api/contact.js
@@ -1,6 +1,6 @@
 import { Resend } from "resend";
 
-const resend = new Resend("re_N1LUJGgW_7f6xxBYFULeCJNzWnqyamDwA");
+const resend = new Resend(RESEND_API_KEY);
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
